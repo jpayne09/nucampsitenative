510aec3812386793164449f3f53ae0a8de1683c6
John Payne <johpayne@geico.com>
2021-11-04 10:31:33 -0400

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,9 @@ import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import Home from './HomeComponent';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -25,7 +28,33 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home},
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle:{
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle:{
+                color: '#fff'
+            }
+        }
+    }
+)
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator},
+        Directory: { screen: DirectoryNavigator}
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+)
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
     render() {

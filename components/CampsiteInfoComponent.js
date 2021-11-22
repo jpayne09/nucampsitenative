84b787ba15a5130f7116838bebadb643c25f79de
John Payne <johpayne@geico.com>
2021-11-22 14:18:48 -0500

    animatable

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import {postFavorite, postComment} from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -41,13 +42,15 @@ function RenderComments({comments}) {
 
     
     return (
-        <Card title='Comments'>
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </Card>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title='Comments'>
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Card>
+        </Animatable.View>
     );
 }
 
@@ -58,6 +61,7 @@ function RenderCampsite(props) {
     console.log(campsite)
     if (campsite) {
         return (
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
             <Card
                 featuredTitle={campsite.name}
                 image={{uri: baseUrl + campsite.image}}>
@@ -84,6 +88,7 @@ function RenderCampsite(props) {
                 />
                 </View>
             </Card>
+            </Animatable.View>
         );
     }
     return <View />;
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {View, Text, ScrollView} from 'react-native';
 import {Card} from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
     constructor(props){
@@ -15,6 +16,7 @@ class Contact extends Component {
     render(){
         return(
             <ScrollView>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
             <Card
             title="Contact Information"
             wrapperStyle={{margin: 20}}
@@ -26,6 +28,7 @@ class Contact extends Component {
             <Text>campsites@nucamp.co</Text>
 
             </Card>
+            </Animatable.View>
             </ScrollView>
         );
     }
